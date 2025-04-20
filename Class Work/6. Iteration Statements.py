a1454a88dd95cf65cb8fb20f381752a8c31f3335
Emad Adnan <90501616+emadadnan000@users.noreply.github.com>
2025-04-20 20:37:31 +0500

    no message

--- Class Work/6. Iteration Statements.py
+++ Class Work/6. Iteration Statements.py
@@ -7,10 +7,10 @@
 for z in range(5,10):
     print(z)
 
-for i in 'name':
+for i in 'faisal amin':
     print(i)         # prints in vertical
 
-for i in 'name':
+for i in 'faisal amin':
     print(i, end=' ') # prints in horizontal
 
 # Sample Program
@@ -20,15 +20,3 @@ full_names = []
 for a_first_name in first_names:
   for a_last_name in last_names:
         full_names.append(a_first_name + " " + a_last_name)
-
-#While Loop
-i = 10
-while i >= 1:
-    print(i)
-    i = i - 1
-
-print("For Loop")
-for i in range(1,11):
-    print(i)
-
-# Sample Program
